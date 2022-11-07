a38822dc14d3d21ae7cbee3926f3067beb109cef
Artjoms Donskojs <100723265+wayarma@users.noreply.github.com>
2022-11-07 09:54:00 +0200

    Create burbspidz.py

---
+++ burbspidz.py
@@ -0,0 +1,89 @@
+from tkinter import *
+from random import randint
+from time import sleep, time
+
+garums = 500
+platums = 800
+
+logs=Tk()
+logs.title("Burbuļu spridzinātājs")
+
+a = Canvas(logs, width=platums, height=garums, bg='darkblue')
+a.pack
+
+kuga_id = a.create_polygon(5, 5, 5, 25, 30, 15, fill="red")
+kuga_id2 = a.create_polygon(0, 0, 30, outline="red")
+
+kuga_r = 15
+vid_x = platums / 2
+vid_y = garums / 2
+
+a.move(kuga_id, vid_x, vid_y)
+a.move(kuga_id2, vid_x, vid_y)
+
+kuga_atr = 10
+
+def parvietot_kugi(notikums):
+    if notikums.keysym == 'Up':
+        a.move(kuga_id, 0, -kuga_atr)
+        a.move(kuga_id2, 0, -kuga_atr)
+    elif notikums.keysym == 'Down':
+        a.move(kuga_id, 0, kuga_atr)
+        a.move(kuga_id2, 0, kuga_atr)
+    elif notikums.keysym == 'Left':
+        a.move(kuga_id, -kuga_atr ,0)
+        a.move(kuga_id2, -kuga_atr, 0)
+    elif notikums.keysym == 'Right':
+        a.move(kuga_id, kuga_atr ,0)
+        a.move(kuga_id2, kuga_atr, 0)
+a.bind_all('<Key>', parvietot_kugi)
+
+burb_id = list()
+burb_r = list()
+burb_atrums = list()
+
+min_burb_r = 10
+max_burb_r = 30
+max_burb_atr = 10
+
+atstarpe = 100
+
+def izveidot_burbuli():
+    x = platums + atstarpe
+    y = randint(0, garums)
+    r = randint(min_burb_r, max_burb_r)
+
+    id1 = a.create_oval(x - r, y -r, x + r, y + r, outline = 'white')
+
+    burb_id.append(id1)
+    burb_r.append(r)
+    burb_atrums.append(randint(1, max_burb_atr))
+
+def parvietot_burbulus():
+    for i in range(len(burb_id)):
+        a.move(burb_id[i], 0)
+
+def iegut_koord(id_skaitlis):
+    poz = a.coords(id_skaitlis)
+    x = (poz[0] + poz[2]) / 2
+    y = (poz[1] + poz[2]) / 2
+    return x,y
+
+def dzest_burbuli(i):
+    del burb_r[i]
+    del burb_atrums[i]
+    a.delete(burb_id[i])
+    del burb_id[i]
+    
+burb_nejausi = 10
+
+#Speles galvenais cikls
+
+while True:
+    if randint(1, burb_nejausi) == 1:
+        izveidot_burbuli()
+        parvietot_burbulus()
+        logs.update()
+        sleep(0.01)
+
+
