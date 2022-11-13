eb2fb649d19816f9e15eb23ea8292eb433f2b054
Artjoms Donskojs <100723265+wayarma@users.noreply.github.com>
2022-11-13 12:18:23 +0000

    Update uzd2.py

--- uzd2.py
+++ uzd2.py
@@ -1,7 +1,8 @@
 from tkinter import *
-from random import randint
+from random import randint #Šis importē funkciju 'randint' no random bibliotēkas.
 
 def ripinat():
+    #Šis kods nodzēš tekstu lauciņā un tajā parāda nejauši izvēlētu skaitli intervālā no 1 līdz 6.
     teksts.delete(0.0, END)
     teksts.insert(END, str(randint(1,6)))
 logs = Tk()
