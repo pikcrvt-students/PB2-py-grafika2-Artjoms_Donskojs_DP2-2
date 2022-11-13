531ff9944fcd07edaa4660801835e429f8142683
Artjoms Donskojs <100723265+wayarma@users.noreply.github.com>
2022-11-13 12:15:50 +0000

    Update uzd1.py

--- uzd1.py
+++ uzd1.py
@@ -1,12 +1,13 @@
 from tkinter import *
 
 def Adarbiba():
-    print("paldies")
+    print("paldies") #Šis ziņojums parādās, kad tiek piespiesta A poga.
 def Bdarbiba():
-    print("Au! man sap!")
+    print("Au! man sap!") #Šis ziņojums parādās, kad tiek piespiesta B poga.
 logs = Tk()
-pogaA = Button(logs, text="Klikšķini te!", command=Adarbiba)
-pogaB = Button(logs, text='Neklikšķini te!', command=Bdarbiba)
+pogaA = Button(logs, text="Klikšķini te!", command=Adarbiba) #Šāds teksts būs redzams uz A pogas
+pogaB = Button(logs, text='Neklikšķini te!', command=Bdarbiba)#Šāds teksts būs redzams uz B pogas
 
+#Šis kods liek datoram ievietot pogas logā.
 pogaA.pack()
 pogaB.pack()
